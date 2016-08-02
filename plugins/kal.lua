b77943ae857d5007f8bccb28825b642cb7eb5e66
Shahab <safaeishahab@yahoo.com>
2016-08-02 16:34:14 +0430

    Update kal.lua

--- plugins/kal.lua
+++ plugins/kal.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  local answers = {'kose nane','nagaidam baw','khar kosde','nane sag','madar sag','nane ghahbe','bi namos','madar jende','kir to namos badkhah tfb','nane lez','namos bakhte','nane geda','nane to sari khor','kiram tfb to kose nanat'}
+  local answers = {'kose nane','nagaidam baw','khar kosde','nane sag','madar sag','nane ghahbe','bi namos','madar jende','kir to namos badkhah tfb','nane lez','namos bakhte','nane geda','nane to sari khor','kiram tfb to kose nanat','nane vel','nane khar','nane lash','nane dast be kos','tamame kire tfb to daro nadaret','badkhah nagaidam lol','kose nanat baw','saghir ta kabireto gaeedam'}
   return answers[math.random(#answers)]
 end
 
